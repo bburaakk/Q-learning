9a171e1470adcab68669817353b2a1a30bb88ab3
bburaakk <149411618+bburaakk@users.noreply.github.com>
2024-09-24 16:53:03 +0300

    Created

--- obj/Release/net8.0/Q learning.AssemblyInfo.cs
+++ obj/Release/net8.0/Q learning.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Q learning")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98586a30ae053453ac7c176f45e7e18ab3438c8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Q learning")]
